8d83629287d54d4124b96437325842f059151000
bachgialight <89329211+bachgialight@users.noreply.github.com>
2025-07-22 15:08:34 +0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import "./i18n";
+import "./i18n.ts";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const GA_MEASUREMENT_ID = "G-9G19PWWEJT";
+
+ReactGA.initialize(GA_MEASUREMENT_ID);
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
